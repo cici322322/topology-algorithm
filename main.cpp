b02c8e5a0526f759e48e276a8a26dffeca5a60a4
cici322322 <43139077+cici322322@users.noreply.github.com>
2019-11-05 10:37:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@ using namespace std;
 using namespace cv;
 
 void  help(){
-	cout<<"    ///////////////////////////////////////////////////////"<<endl
-		<<"start to simulate wsn"<<endl
+	cout    <<"start to simulate wsn"<<endl
 		<<"random 1-10 nodes send signal"<<endl
 		<<"the color of nodes represents ResEnergy"<<endl
 		<<"interval of two flashingrepresents one unit timeT"<<endl
@@ -16,38 +15,37 @@ void  help(){
 		<<"yellow means 75% >= ResEnergy > 56%"<<endl
 		<<"orange means 56% >= ResEnergy > 42%"<<endl
 		<<"red means 42% > =ResEnergy > 31%"<<endl
-		<<"grey means 31% >= ResEnergy > 235%"<<endl
+		<<"grey means 31% >= ResEnergy > 23%"<<endl
 		<<"black means 23% > =ResEnergy >  0 "<<endl
 		<<"1 represents constant TX to leader nodes"<<endl
 		<<"2 represents variable TX to leader nodes"<<endl
-		<<"--"<<endl
 		<<"select corresponedent number";
 }
 
 void main()
 {
 	help();
-	WSNTopologyBasedEnergy wsntoplogy;
+	Energy toplogy;
 	char c = getchar();
 	switch (c)
 	{
 	case '1':
-		wsntoplogy = WSNTopologyBasedEnergy(WSNTopologyBasedEnergy::ConstPowertoLeader);
+		toplogy = Energy(Energy::ConstTXtoLeader);
 		break;
 	case '2':
-		wsntoplogy = WSNTopologyBasedEnergy(WSNTopologyBasedEnergy::VarPowertoLeaderAndSinkShift);
+		toplogy = Energy(Energy::VarTXtoLeader);
 		break;
 	default:
 		cout<<"constant TX"<<endl;
 		break;
 	}
 	cout<<"continue"<<endl;
-	wsntoplogy.InitNode(600,600,100);
-	wsntoplogy.ClassifyNode();
-	wsntoplogy.DrawWSNLayer();
-	wsntoplogy.FindLayerFirstLinkNode(); 	
-	wsntoplogy.LinkNodeEachLayer();
-	wsntoplogy.WSNLifeTime();
+	toplogy.InitNode(600,600,100);
+	toplogy.ClassifyNode();
+	oplogy.DrawLayer();
+	toplogy.FindLayerFirstLinkNode(); 	
+	toplogy.LinkNodeEachLayer();
+	toplogy.WSNTime();
 
 	waitKey();
 } 
