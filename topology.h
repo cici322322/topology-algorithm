01a7d9235c7e6b4d1a57aa4ec8ea811751b0029f
cici322322 <43139077+cici322322@users.noreply.github.com>
2019-11-05 10:31:40 +0100

    Update topology.h

--- topology.h
+++ topology.h
@@ -7,45 +7,29 @@
 using namespace std;
 using namespace cv;
 
-class WSNTopologyBasedEnergy
+class Energy
 {
-public:
-	enum TransportType
-	{
-		ConstPowertoLeader = 0,
-		VarPowertoLeaderAndSinkShift = 1,
-	};
-
-public:
-	WSNTopologyBasedEnergy(TransportType type = ConstPowertoLeader);
-	int InitNode(int width, int heigth,  int NodeNum);
-	void ClassifyNode();
-	void DrawWSNLayer();
-	void FindLayerFirstLinkNode();
-	void LinkNodeEachLayer();
-	int WSNLifeTime();
-
 private:
-	void drawCrossNode(Mat &img, WSNNode node, Scalar color);
-	double DistanceBetweenNodes(WSNNode node1,WSNNode node2);
-	double SDistanceBetweenNodes(WSNNode node1,WSNNode node2);
+	void drawCrossNode(Mat &img, Node node, Scalar color);
+	double DistanceBetweenNodes(Node node1,Node node2);
+	double SDistanceBetweenNodes(Node node1,Node node2);
 	vector<int> SelectServalNode();
 	void SelectLeaderID();
-	void TopLayer0SentSink();
-	void TopLayer1SentSink();
-	void TopLayer2SentSink();
-	void TopLayer3SentSink();
-	void TopLayer4SentSink();
+	void TopLayer0Sink();
+	void TopLayer1Sink();
+	void TopLayer2Sink();
+	void TopLayer3Sink();
+	void TopLayer4Sink();
 
 	int MinMark();
-	void NodeSentSignalVarPower();
-	void NodeSentSignalSamePower();
+	void NodeSentSignalVarTX();
+	void NodeSentSignalConstTX();
 
 private:
 	Mat img;
 	bool Test
 	char type
-	WSNNode sink;
+	Node sink;
 	double alpha;
 	int TopLayer;
 	static const int NODENUM = 100;
@@ -54,9 +38,27 @@ private:
 	double NetThresEnergy;
 	int Mark[LayerNum];
 	double ThresEnergy[LayerNum];
-	WSNNode node[NODENUM];
-	WSNNode LayerFirstLinkNode[LayerNum];
-	vector<WSNNode> layer[LayerNum]; 
-	vector<WSNNode> LayerLeaderSeq[LayerNum];
-	WSNNode LayerLeaderNode[LayerNum];
+	Node node[NODENUM];
+	Node LayerFirstLinkNode[LayerNum];
+	vector<Node> layer[LayerNum]; 
+	vector<Node> LayerLeaderSeq[LayerNum];
+	Node LayerLeaderNode[LayerNum];
+	
+public:
+	enum TransportType
+	{
+		ConstTX = 0,
+		VarTX = 1,
+	};
+
+public:
+        Energy(TransportType type = ConstTX);
+	int InitNode(int width, int heigth,  int NodeNum);
+	void ClassifyNode();
+	void DrawLayer();
+	void FindFirstLinkNode();
+	void LinkNodeEachLayer();
+	int LifeTime();
+
+
 };
